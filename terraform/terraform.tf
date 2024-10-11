99a9c844f2c70eb07953e1af6cbe06389522fccf
v-pratnakara <v-pratnakara@microsoft.com>
2024-10-11 10:13:38 +0530

    added the version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 0.13.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.4.0"
+      version = "3.30.0"
     }
   }
 
