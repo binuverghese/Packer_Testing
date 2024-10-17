a7d75f47407d54c5fcd8cfde7acc8909db64100d
v-pratnakara <v-pratnakara@microsoft.com>
2024-10-17 10:55:47 +0530

    update the sh script fecth pvt ip of vm

--- terraform-poc/create_inventory.sh
+++ terraform-poc/create_inventory.sh
@@ -2,13 +2,13 @@
 
 # Get the private IP from Terraform output
 vm_private_ip=$(terraform output -raw vm_private_ip)
-
+echo "Private IP fetched: $VM_PRIVATE_IP"
 # Check if PRIVATE_IP is not empty
 if [ -z "$vm_private_ip" ]; then
     echo "Error: Could not retrieve the private IP from Terraform output."
     exit 1
 fi
-
+echo "Private IP fetched: $VM_PRIVATE_IP"
 # Create the Ansible inventory file
 cat <<EOL > inventory.ini
 [windows]
