0e3023d2c7d8558e61b0b3553432032a98e18288
v-pratnakara <v-pratnakara@microsoft.com>
2024-10-17 10:46:32 +0530

    added the condition in sh script

--- terraform-poc/create_inventory.sh
+++ terraform-poc/create_inventory.sh
@@ -3,10 +3,16 @@
 # Get the private IP from Terraform output
 vm_private_ip=$(terraform output -raw vm_private_ip)
 
+# Check if PRIVATE_IP is not empty
+if [ -z "$vm_private_ip" ]; then
+    echo "Error: Could not retrieve the private IP from Terraform output."
+    exit 1
+fi
+
 # Create the Ansible inventory file
 cat <<EOL > inventory.ini
 [windows]
-$vm_private_ip ansible_user=adminuser ansible_password=Welcome#2024 ansible_connection=winrm ansible_port=5986 ansible_winrm_transport=ntlm ansible_winrm_server_cert_validation=ignore
+$vm_private_ip ansible_user=adminuser ansible_password="Welcome#2024" ansible_connection=winrm ansible_port=5986 ansible_winrm_transport=ntlm ansible_winrm_server_cert_validation=ignore
 EOL
 
 echo "Inventory file created at ansible/inventory.ini"
